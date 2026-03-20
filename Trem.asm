afdc70cdbdb4421931d15cdb1bf52ef19f3de778
Arthur de Castro Dias <148163803+ArthDias06@users.noreply.github.com>
2026-03-20 12:48:23 -0300

    Melhoria do código

--- Trem.asm
+++ Trem.asm
@@ -2,10 +2,8 @@
 	.align 0
 Welcome:.asciz "-----------Vamos começar o Jogo-----------"
 menu:   .asciz "\n\nComo jogar:\n1-Adiconar vagão no início\n2-Adiconar vagão no final\n3-Remover vagão por ID\n4-Listar trem\n5-Buscar vagão\n6-Sair\nDigite sua escolha: "
-error:	.asciz "Erro ao processar input\nPressiona qualquer tecal para continuar "
+error:	.asciz "Erro ao processar input\nPressiona qualquer tecla para continuar "
 goodbye:.asciz "-----------Obrigado por jogar!-----------"
-	.align 1
-option: .space 2
 
 	.text
 	.align 2
@@ -14,22 +12,44 @@ main:
 	la a0, Welcome
 	addi a7, zero, 4 #imprime o valor de Welcome na tela
 	ecall
-	addi t0, zero, 1
-	addi t1, zero, 6
-game:   la a0, menu
+
+game:   la a0, menu #Carrega o menu para o usuário
 	addi a7, zero, 4
 	ecall
-	addi a7, zero, 5
+	addi a7, zero, 5 #Lê input do usuário
 	ecall
-	blt a0, t0, errorM
-	bgt a0, t1, errorM
-	beq a0, t1, case6
+	addi t0, zero, 1
+	blt a0, t0, errorM # Se input menor que 1 printa mensagem de erro
+	beq a0, t0, case1
+	addi t0, zero, 6
+	bgt a0, t0, errorM#Se input maior que 1 printa mensagem de erro
+	beq a0, t0, case6
+	addi t0, zero, 2
+	beq a0, t0, case2
+	addi t0, zero, 3
+	beq a0, t0, case3
+	addi t0, zero, 4
+	beq a0, t0, case4
+	addi t0, zero, 5
+	beq a0, t0, case5
+#Adiciona vagão no início
+case1:	
+#Adiciona vagão no final
+case2:
+#Remove vagão por ID
+case3:
+#Listar todos os vagões
+case4:	
+#Busca pelo ID
+case5:	
+#Encerra o programa
 case6:	la a0, goodbye
 	addi a7, zero, 4
 	ecall
-	addi a7, zero, 10 #Encerra o programa
+	addi a7, zero, 10
 	ecall
-errorM:	la a0, error
+	
+errorM:	la a0, error#Imprime mensagem de erro
 	addi a7, zero, 4
 	ecall
 	addi a7, zero, 12
