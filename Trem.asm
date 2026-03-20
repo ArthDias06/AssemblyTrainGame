a5ec13c51da0a7ffad495ad7f0fb3b4f0238a229
Arthur de Castro Dias <148163803+ArthDias06@users.noreply.github.com>
2026-03-20 13:38:00 -0300

    Início alocação de memória

--- Trem.asm
+++ Trem.asm
@@ -1,6 +1,6 @@
 	.data
 	.align 0
-Welcome:.asciz "-----------Vamos começar o Jogo-----------"
+welcome:.asciz "-----------Vamos começar o Jogo-----------"
 menu:   .asciz "\n\nComo jogar:\n1-Adiconar vagão no início\n2-Adiconar vagão no final\n3-Remover vagão por ID\n4-Listar trem\n5-Buscar vagão\n6-Sair\nDigite sua escolha: "
 error:	.asciz "Erro ao processar input\nPressiona qualquer tecla para continuar "
 goodbye:.asciz "-----------Obrigado por jogar!-----------"
@@ -9,7 +9,16 @@ goodbye:.asciz "-----------Obrigado por jogar!-----------"
 	.align 2
 	.globl main
 main:
-	la a0, Welcome
+	addi a0, zero, 12 # Serão alocados 12 bytes(4 para cada campo)
+	addi a7, zero, 9 #Alocação de memória na heap, criando a cabeça da lista ligada
+	ecall
+	add s0, zero, a0 #Passa o conteúdo de a0 para s0
+	addi s1, zero, -1
+	sw s1, 0(s0) #A cabeça começa com o ID -1, ela não poderá ser acessada pelo usuário
+	sw s1, 4(s0) # O tipo da cabeça é -1 também
+	addi s1, zero, 1
+	sw s1, 8(s0) #ponteiro nulo no primeiro momento
+	la a0, welcome
 	addi a7, zero, 4 #imprime o valor de Welcome na tela
 	ecall
 
@@ -33,7 +42,7 @@ game:   la a0, menu #Carrega o menu para o usuário
 	addi t0, zero, 5
 	beq a0, t0, case5
 #Adiciona vagão no início
-case1:	
+case1:
 #Adiciona vagão no final
 case2:
 #Remove vagão por ID
