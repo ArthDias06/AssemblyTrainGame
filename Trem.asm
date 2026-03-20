b86f7d37791f28f1ec7905004b35e3e66b6cc53c
Arthur de Castro Dias <148163803+ArthDias06@users.noreply.github.com>
2026-03-20 18:01:20 -0300

    Funções de buscar e inserir início Começadas

--- Trem.asm
+++ Trem.asm
@@ -2,7 +2,11 @@
 	.align 0
 welcome:.asciz "-----------Vamos começar o Jogo-----------"
 menu:   .asciz "\n\nComo jogar:\n1-Adiconar vagão no início\n2-Adiconar vagão no final\n3-Remover vagão por ID\n4-Listar trem\n5-Buscar vagão\n6-Sair\nDigite sua escolha: "
-error:	.asciz "Erro ao processar input\nPressiona qualquer tecla para continuar "
+error:	.asciz "\nErro ao processar input\n"
+press:	.asciz "Pressiona qualquer tecla para continuar: "
+miss:	.asciz "\nID não encontrado!\n"
+types:	.asciz "\n1-Locomotiva\n2-Carga\n3-Passageiro\n4-Combustível\nDigite o tipo do vagão: "
+idSeek:	.asciz "\nDigite o ID desejado para a procura: "
 goodbye:.asciz "-----------Obrigado por jogar!-----------"
 
 	.text
@@ -17,21 +21,22 @@ main:
 	sw s1, 0(s0) #A cabeça começa com o ID -1, ela não poderá ser acessada pelo usuário
 	sw s1, 4(s0) # O tipo da cabeça é -1 também
 	addi s1, zero, 1
-	sw s1, 8(s0) #ponteiro nulo no primeiro momento
+	sw s0, 8(s0) #lista circular
 	la a0, welcome
 	addi a7, zero, 4 #imprime o valor de Welcome na tela
 	ecall
 
-game:   la a0, menu #Carrega o menu para o usuário
+game:   lw s3, 8(s0)
+	la a0, menu #Carrega o menu para o usuário
 	addi a7, zero, 4
 	ecall
 	addi a7, zero, 5 #Lê input do usuário
 	ecall
 	addi t0, zero, 1
-	blt a0, t0, errorM # Se input menor que 1 printa mensagem de erro
+	blt a0, t0, errorM #Se input menor que 1 printa mensagem de erro
 	beq a0, t0, case1
 	addi t0, zero, 6
-	bgt a0, t0, errorM#Se input maior que 1 printa mensagem de erro
+	bgt a0, t0, errorM #Se input maior que 1 printa mensagem de erro
 	beq a0, t0, case6
 	addi t0, zero, 2
 	beq a0, t0, case2
@@ -43,14 +48,53 @@ game:   la a0, menu #Carrega o menu para o usuário
 	beq a0, t0, case5
 #Adiciona vagão no início
 case1:
+	la a0, types#Print do menu
+	addi a7, zero, 4
+	ecall
+	addi a7, zero, 5#Recebimento do input do usuário
+	ecall
+	addi t0, zero, 1
+	blt a0, t0, errorM
+	addi t0, zero, 4
+	bgt a0, t0, errorM
+	add s2, zero, a0
+	addi a0, zero, 12#Alocação da memória
+	addi a7, zero, 9
+	ecall
+	sw s1, 0(a0)#ID do novo item
+	addi, s1, zero, 1 #Aumento o contador do id
+	sw s2, 4(a0)#Tipo do novo item
+	lw t0, 8(s0) #Agora o novo item aponta para o endereço que aponta s0
+	sw t0, 8(a0)
+	sw a0, 8(s0) #s0 aponta para o endereço do novo item
+	j game
 #Adiciona vagão no final
 case2:
+	
 #Remove vagão por ID
 case3:
+	
 #Listar todos os vagões
-case4:	
+case4:
+
 #Busca pelo ID
-case5:	
+case5:
+	la a0, idSeek
+	addi a7, zero, 4
+	ecall
+	addi a7, zero, 5
+	ecall
+	blt a0, zero, errorM
+seek:	lw s3, 8(s3)
+	beq s0, s3, noID
+	lw t0, 0(s3)
+	bne t0, a0, seek
+	lw a0, 0(s3)
+	addi a7, zero, 1
+	lw a0, 4(s3)
+	addi a7, zero, 1
+	j game
+	
 #Encerra o programa
 case6:	la a0, goodbye
 	addi a7, zero, 4
@@ -59,6 +103,19 @@ case6:	la a0, goodbye
 	ecall
 	
 errorM:	la a0, error#Imprime mensagem de erro
+	addi a7, zero, 4
+	ecall
+	la a0, press
+	addi a7, zero, 4
+	ecall
+	addi a7, zero, 12
+	ecall
+	j game
+
+noID:	la a0, miss
+	addi a7, zero, 4
+	ecall
+	la a0, press
 	addi a7, zero, 4
 	ecall
 	addi a7, zero, 12
