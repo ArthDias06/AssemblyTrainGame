901ce975d051dd9c4e9c965bec00a65bab0f3406
Arthur de Castro Dias <148163803+ArthDias06@users.noreply.github.com>
2026-03-26 15:34:24 -0300

    Uso de pilha nas funções

--- Trem.asm
+++ Trem.asm
@@ -1,24 +1,26 @@
 #Arthur de Castro Dias - 16855302
 #Cleyton José - 16
-#Felipe Gausmann Socolowski - 16
+#Felipe Gausmann Socolowski - 16812461
 #Gabriel Carraro Salzedas - 16827905
 
 
 #Descrição dos registradores usados
 #s0 - Armazenamento do endereço da cabeça da lista
 #s1 - registrador contendo o próximo ID
-#s2 - Registrador usado na inserção no início
 #s3 - Registrador usado para percorrer a lista
 #s4 - Armazena o contador do vagão na listagem e na busca
 #t0 - usado para armazenar valor a ser comparado(nas instruções bne e beq)
 #t1 - Armazena o valor do tipo do vagão na busca
 #t2 - Verifica o próximo nó para saber se ele foi escolhido para ser removido
 #t3 - Armazena o valor do ID do próximo nó para saber se ele será removido e armazena o valor do nó próximo de t2 para passar para o s3
+#a0 - Parâmetro de retorno da função setInfo
+#a1, a2, a3 - Parâmetros de chamada da função putInfo
+#sp - stack pointer, controla a pilha
 	.data
 	.align 0
 	
 #Definição das strings usadas para melhora do ambiente
-welcome:.asciz "-----------Vamos começar o Jogo-----------"
+welcome:.asciz "\n-----------Vamos começar o Jogo-----------"
 menu:   .asciz "\n\nComo jogar:\n1-Adiconar vagão no início\n2-Adiconar vagão no final\n3-Remover vagão por ID\n4-Listar trem\n5-Buscar vagão\n6-Sair\nDigite sua escolha: "
 error:	.asciz "\nErro ao processar input"
 press:	.asciz "\nPressiona qualquer tecla para continuar: "
@@ -36,6 +38,7 @@ list:	.asciz "\n-----------Listando-----------"
 idPrint:.asciz "\nID do vagão: "
 tyPrint:.asciz "\nTipo do vagão: "
 wagon:	.asciz "\n\nVagão "
+idCount:.word 0
 
 	.text
 	.align 2
@@ -48,22 +51,23 @@ main:
 	ecall
 	#s0 aponta para a cabeça do trem
 	add s0, zero, a0
-	addi s1, zero, -1
+	addi t0, zero, -1
 	#A cabeça começa com o ID -1, ela não poderá ser acessada pelo usuário
-	sw s1, 0(s0)
+	sw t0, 0(s0)
 	# O tipo da cabeça é -1 também
-	sw s1, 4(s0)
-	addi s1, s1, 1
+	sw t0, 4(s0)
 	#lista circular
 	sw s0, 8(s0)
+	#s3 servirá para percorrer a lista em diversas situações, ele começa sempre apontando o nó cabeça
+	add s3, zero, s0
+	#Contador do número de vagão
+	addi s4, zero, 0
 	#Início da UI do jogo
 	la a0, welcome
 	addi a7, zero, 4
 	ecall
 
-game:	#s3 servirá para percorrer a lista em diversas situações, sempre ao voltar ele volta a apontar para o nó cabeça
-	add s3, zero, s0
-	#Carrega o menu para o usuário
+game:	#Carrega o menu para o usuário
 	la a0, menu
 	addi a7, zero, 4
 	ecall
@@ -90,8 +94,6 @@ game:	#s3 servirá para percorrer a lista em diversas situações, sempre ao vol
 
 #Adiciona vagão no início
 case1:	jal ra, setInfo
-	#Tipo do novo item
-	sw s2, 4(a0)
 	#Agora o novo item aponta para o endereço que apontava s0
 	lw t0, 8(s0)
 	sw t0, 8(a0)
@@ -102,20 +104,29 @@ case1:	jal ra, setInfo
 
 #Adiciona vagão no final
 case2:
-	lw s3, 8(s3) #Percorre a lista
+	#Empilha o s3
+	addi sp, sp, -4
+	sw s3, 0(sp)
+	#Percorre a lista
+	lw s3, 8(s3)
 	lw t0, 8(s3)
 	#Verifica se a lista chegou no final
 	bne s0, t0, case2
 	#Vai ao case1 para coleta de informações e criação do nó
 	jal ra, setInfo
-	sw s2, 4(a0)
 	sw a0, 8(s3)
 	sw s0, 8(a0)
+	#Desempilha o s3
+	lw s3, 0(sp)
+	addi sp, sp, 4
 	#Retorno ao menu do jogo
 	j game
 
 #Remove vagão por ID
 case3:
+	#Empilha s3
+	addi sp,sp, -4
+	sw s3, 0(sp)
 	la a0, idKill
 	addi a7, zero, 4
 	ecall
@@ -139,34 +150,48 @@ check:	lw t2, 8(s3)
 remove:	lw t3, 8(t2)
 	#O nó atual aponta para o nó que t2 apontava
 	sw t3, 8(s3)
-	#t3 contém 0
-	addi t3, zero, 0
 	#t2 aponta para NULL
-	sw t3, 8(t2)
+	sw zero, 8(t2)
+	#Desempilhar s3
+	lw s3, 0(sp)
+	addi sp,sp,4
 	#Retorno ao menu do jogo
 	j game
 
 #Listar todos os vagões
 case4:
-	#Inicia o contador de vagões
-	addi s4, zero, 1
+	#Empilha s3 e s4
+	addi sp,sp, -8
+	sw s3, 0(sp)
+	sw s4, 4(sp)
 	la a0, list
 	addi a7, zero, 4
 	ecall
 listing:lw s3, 8(s3)
 	#Se s0 = s3 ele deu uma volta e terminou a listagem
-	beq s0, s3, print
-	#Vai para a função de printar informações do vagão
-	jal ra, putInfo
+	beq s0, s3, endList
+	#Definição dos parâmetros para a função
 	#Aumento do contador do vagão
 	addi s4, s4, 1
+	add a1, zero, s4
+	lw a2, 0(s3)
+	lw a3, 4(s3)
+	#Vai para a função de printar informações do vagão
+	jal ra, putInfo
 	#Retorna para a listagem
 	j listing
+	#Desempilha o s3 e s4
+endList:lw s3, 0(sp)
+	lw s4, 4(sp)
+	addi sp, sp, 8
+	j print
 
 #Busca pelo ID
 case5:
-	#Contador do número de vagão
-	addi s4, zero, 0
+	#Empilha s3 e s4
+	addi sp,sp, -8
+	sw s3, 0(sp)
+	sw s4, 4(sp)
 	la a0, idSeek
 	addi a7, zero, 4
 	ecall
@@ -186,7 +211,15 @@ seek:	lw s3, 8(s3)
 	la a0, found
 	addi a7, zero, 4
 	ecall
+	#Definição dos parâmetros da função
+	add a1, zero, s4
+	lw a2, 0(s3)
+	lw a3, 4(s3)
 	jal ra, putInfo
+	#Desempilha o s3
+	lw s3, 0(sp)
+	lw s4, 4(sp)
+	addi sp, sp, 8
 	j print
 
 #Encerra o programa
@@ -211,29 +244,37 @@ setInfo:#Print do menu de tipos
 	addi t0, zero, 4
 	#Se o input for maior que 4 dá erro
 	bgt a0, t0, errorM
-	add s2, zero, a0
+	add a1, zero, a0
 	addi a0, zero, 12
 	#Alocação de memória do novo nó
 	addi a7, zero, 9
 	ecall
+	la t0, idCount
+	#Pega o valor de idCount
+	lw t1, 0(t0)
 	#ID do novo item
-	sw s1, 0(a0)
+	sw t1, 0(a0)
 	#Aumento o contador do id
-	addi s1, s1, 1
+	addi t1, t1, 1
+	#Guarda o vaor do item
+	sw t1, 0(t0)
+	#Tipo do novo item
+	sw a1, 4(a0)
 	jr ra
 
 #Print das informações do vagão
-putInfo:la a0, wagon
+putInfo:#Print do número do vagão
+	la a0, wagon
 	addi a7, zero, 4
 	ecall
-	add a0, zero, s4
+	add a0, zero, a1
 	addi a7, zero, 1
 	ecall
 	#Print do ID do vagão
 	la a0, idPrint
 	addi a7, zero, 4
 	ecall
-	lw a0, 0(s3)
+	add a0, zero, a2
 	addi a7, zero, 1
 	ecall
 	#Print do tipo do vagão
@@ -242,23 +283,22 @@ putInfo:la a0, wagon
 	ecall
 	#Print do tipo por extenso
 	#Print do tipo locomotiva
-	lw t1, 4(s3)
 	addi t0, zero, 1
-	bne t0, t1, load
+	bne t0, a3, load
 	la a0, locomot
 	addi a7, zero, 4
 	ecall
 	jr ra
 	#Print do tipo Carga
 load:	addi t0, zero, 2
-	bne t1, t0, person
+	bne a3, t0, person
 	la a0, cargo
 	addi a7, zero, 4
 	ecall
 	jr ra
 	#Print do tipo passageiro
 person:	addi t0, zero, 3
-	bne t1, t0, gas
+	bne a3, t0, gas
 	la a0, passeng
 	addi a7, zero, 4
 	ecall
@@ -279,6 +319,9 @@ errorM:	la a0, error
 noID:	la a0, miss
 	addi a7, zero, 4
 	ecall
+	#Desempilhar s3
+	lw s3, 0(sp)
+	addi sp,sp,4
 	j print
 
 #Mensagem base para o usuário apertar uma tecla para continuar
